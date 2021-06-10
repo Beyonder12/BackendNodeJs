ce875062e351bb5a3572acc798fed63516f611c1
Fajr <fajri.illahi@medicaboo.com>
2021-06-11 06:44:28 +0700

    Implement  basic query

---
+++ problem1.sql
@@ -0,0 +1,17 @@
+/*
+Simple Database Querying
+
+There are USER Table which has 3 column : ID, UserName, Parent. Where:
+ID column is primary key
+UserName column is name of user
+Parent column is ID from User which is creator for certain User
+
+*/
+
+
+/*
+Here the solution
+Assumption there is USER Table and CREATOR Table
+*/
+
+SELECT ID, UserName, ParentUserName FROM USER INNER JOIN CREATOR ON USER.PARENT = CREATOR.ID;
