1d9b0a856e29280d98ccdf1ba7bbb3b0dc5b5ac9
Fajr <fajri.illahi@medicaboo.com>
2021-06-11 02:19:02 +0700

    testing the original code

--- problem3.js
+++ problem3.js
@@ -1,1 +1,28 @@
-//tes
+//This problem to refactoring this code
+
+function findFirstStringInBracket(str) {
+  if(str.length > 0) {
+    let indexFirstBracketFound = str.indexOf("(");
+    if(indexFirstBracketFound >= 0) {
+      let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
+      if(wordsAfterFirstBracket) {
+        wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
+        let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+        if(indexClosingBracketFound >= 0) {
+          return wordsAfterFirstBracket.substr(0, indexClosingBracketFound);
+        } else {
+          return "";
+        }
+      } else {
+        return "";
+      }
+    } else {
+      return "";
+    }
+  } else {
+    return "";
+  }
+}
+
+console.log(findFirstStringInBracket("aa((bc)")); //(bc
+console.log(findFirstStringInBracket("(abc))")); //abc
