ed8eff0960747e9bcdff5241ad9cf401ac352bad
Fajr <fajri.illahi@medicaboo.com>
2021-06-11 07:01:53 +0700

    problem 4 logic test

---
+++ problem4.js
@@ -0,0 +1,18 @@
+/*
+ Logic Test
+ Anagram is a term where a string if shuffle will be same. example
+ 'aku' and 'kua' is anagram. simply, anagram is 2 string with same length
+ and count of each char.
+
+
+ input = ['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua']
+
+ the output of the input array above
+
+ output = [
+   ['kita', 'atik', 'tika'],
+   ['aku', 'kua'],
+   ['makan'],
+   ['kia']
+ ]
+ */
