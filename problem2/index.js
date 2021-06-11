b7dc08ef59fe695f762713a5f81ee7a1a38f6342
Fajr <fajri.illahi@medicaboo.com>
2021-06-11 10:30:48 +0700

    express

--- problem2/index.js
+++ problem2/index.js
@@ -7,4 +7,17 @@ app.get("/", (request, response) => {
   response.end("Home page");
 })
 
+app.get('/getUsersAPI', (request, response) => {
+
+})
+
+app.get('/getUsersAPI', (request, response) => {
+  fs.readFile(__dirname+"/data/users.json", "utf-8", (err, results) => {
+    if(err) {
+      throw err;
+    } else {
+      response.end();
+    }
+  })
+})
 app.listen(port);
